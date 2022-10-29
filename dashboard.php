ca51c74785dcbb4cfb0ef3f179af62fb83bd769d
Dave M <dmurray19@gmail.com>
2022-10-29 06:11:18 -0500

    Changed dashboard layout

--- dashboard.php
+++ dashboard.php
@@ -19,6 +19,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <title>CreditWatch - Fraud Dashboard</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/sidebars.css" rel="stylesheet">
+    <link href="css/dashboard.css" rel="stylesheet">
 </head>
 <body>
 
@@ -48,10 +49,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       </symbol>
     </svg>
 
-
-<div class="row">
-  <div class="col">
-    <div class="d-flex flex-nowrap">
+    <main class="d-flex flex-nowrap">
         
       <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
@@ -105,28 +103,101 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
           </ul>
         </div>
       </div>
-    
 
-      <iframe id="idIframe" onload="iframeLoaded()" src="dashboard_frame.html" width="1000">
-         Sorry your browser does not support inline frames.
-      </iframe>
 
-</div>
-    </div>
-  </div>
+            <div class="container-fluid">
+              <div class="col-md-4">
+                <div class="pt-5">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Fraud Details</h1>
+                    </div>
+
+                    <div class="row">
+
+                        <!-- total fraud found for account -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                Total Fraud</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">12</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- total fraud within the last month -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Monthly Fraud</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">5</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- percentage of fraud against number of records checked -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-info shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Percent Fraud
+                                            </div>
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col-auto">
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">20%</div>
+                                                </div>
+                                                <div class="col">
+                                                    <div class="progress progress-sm mr-2">
+                                                        <div class="progress-bar bg-info" role="progressbar"
+                                                            style="width: 20%" aria-valuenow="20" aria-valuemin="0"
+                                                            aria-valuemax="100"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- total number of transactions checked for fraud -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-warning shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                                Transactions Checked</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">240</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+
+
+</main>
 
     <script src="js/bootstrap.bundle.min.js"></script>
     <script src="js/sidebars.js"></script>
-    <script type="text/javascript">
-      function iframeLoaded() {
-          var iFrameID = document.getElementById('idIframe');
-          if(iFrameID) {
-                // here you can make the height, I delete it first, then I make it again
-                iFrameID.height = "";
-                iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
-          }   
-      }
-    </script>   
 
 </body>
 </html>
