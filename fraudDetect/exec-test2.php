536187e01e1cb71574ae03879b928e8c8bf2dd46
Dave M <dmurray19@gmail.com>
2022-11-06 07:17:47 -0600

    add remote server files used fraudDetect processing

---
+++ fraudDetect/exec-test2.php
@@ -0,0 +1,12 @@
+<?php
+
+	$tobecopied = 'https://www.ohmydar.win/creditwatch/data/transactions.csv';
+	$target = $_SERVER['DOCUMENT_ROOT'] . '/fraudDetect/transactions.csv';
+
+	copy($tobecopied, $target);
+	
+	$fraudDetect = shell_exec("./fraudDetect transactions.csv");
+	echo $fraudDetect;
+
+
+?>
---
+++ fraudDetect/getFile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl http://www.ohmydar.win/creditwatch/data/transactions.csv --output /var/www/remoteto.us/html/fraudDetect/transactions.csv --silent
